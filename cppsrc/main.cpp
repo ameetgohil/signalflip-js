eacd03ea73659fb77ed9015df86b4dab3b2c1d8c
qubits <ameet@siglabs.com>
2019-01-26 15:44:46 -0800

    clean up

--- cppsrc/main.cpp
+++ cppsrc/main.cpp
@@ -1,10 +1,3 @@
-// DESCRIPTION: Verilator: Verilog example module
-//
-// This file ONLY is placed into the Public Domain, for any use,
-// without warranty, 2017 by Wilson Snyder.
-//======================================================================
-
-// Include common routines
 #include <napi.h>
 
 Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
