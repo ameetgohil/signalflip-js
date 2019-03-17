7feb693cc9bf9549928487503916ebeb5e73ca02
Ameet Gohil <qubits1@gmail.com>
2019-03-16 22:46:06 -0700

    add generatewrapper to index.js exports

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports = {
     Sim: require('./src/Sim').Sim,
     RisingEdge: require('./src/Sim').RisingEdge,
     FallingEdge: require('./src/Sim').FallingEdge,
-    Interfaces: require('./src/interfaces')
+    Interfaces: require('./src/interfaces'),
+    GenerateWrapper: require('./src/GenerateWrapper')
 //    GenerateWrapper: require('./src/generate-wrapper')
 };
 
