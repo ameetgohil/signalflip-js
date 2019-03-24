75550045fc4e3b77b2d7afa81869352896ca5562
qubits <ameet@siglabs.com>
2019-03-23 22:26:24 -0700

    add clock

---
+++ src/sim-utils/Clock.js
@@ -0,0 +1,18 @@
+function Clock(sig, halfPeriod) {
+    this.enable = 1;
+    this.clk = function* () {
+	let value = false
+	sig(value ? 1:0);
+	while(true) {
+	    for(i of _.range(halfPeriod)) {
+		yield* Tick();
+		value = !value;
+		if(enable) {
+		    sig(value ? 1:0);
+		}
+	    }
+	}
+    }
+}
+
+export.modules = Clock;
---
+++ src/sim-utils/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    Clock: require('./Clock')
+};
