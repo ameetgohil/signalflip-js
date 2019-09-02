96421b4d53b243e205d1570773bc400f1b817439
qubits <ameet@siglabs.com>
2019-09-02 01:32:39 -0700

    add Fork export

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 module.exports = {
-    Sim: require('./src/Sim').Sim,
-    SimUtils: require('./src/sim-utils'),
-    RisingEdge: require('./src/Sim').RisingEdge,
-    RisingEdges: require('./src/Sim').RisingEdges,
-    FallingEdge: require('./src/Sim').FallingEdge,
-    FallingEdgese: require('./src/Sim').FallingEdges,
-    Interfaces: require('./src/interfaces'),
-    GenerateWrapper: require('./src/GenerateWrapper')
+  Sim: require('./src/Sim').Sim,
+  SimUtils: require('./src/sim-utils'),
+  RisingEdge: require('./src/Sim').RisingEdge,
+  RisingEdges: require('./src/Sim').RisingEdges,
+  FallingEdge: require('./src/Sim').FallingEdge,
+  FallingEdgese: require('./src/Sim').FallingEdges,
+  Fork: require('./src/Sim').Fork,
+  Interfaces: require('./src/interfaces'),
+  GenerateWrapper: require('./src/GenerateWrapper')
 };
 
 
